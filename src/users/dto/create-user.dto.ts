17923ba1a66cafad514ea4c3e4a203be341c7e43
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-03-10 00:20:07 +0300

    docs: document dto fields in swagger

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,9 @@
+import {ApiProperty} from "@nestjs/swagger";
+
 export class CreateUserDto {
+    @ApiProperty({example: 'user@mail.ru', description: 'User email'})
     readonly email: string;
+
+    @ApiProperty({example: '12334241', description: 'User password'})
     readonly password: string;
 }
