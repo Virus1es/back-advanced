ade997331f1ab3bc08aa09afe4d84f3ad12c1d9d
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-03-09 23:37:23 +0300

    fix: fix id error

--- src/users/users.model.ts
+++ src/users/users.model.ts
@@ -8,7 +8,7 @@ interface UserCreationAttrs {
 @Table({tableName: 'users'})
 export class User extends Model<User, UserCreationAttrs> {
     @Column({type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true})
-    id: number;
+    declare id: number;
 
     @Column({type: DataType.STRING, unique: true, allowNull: false})
     email: string;
