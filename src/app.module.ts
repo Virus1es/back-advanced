58c6797a3de6ec3683367a9131290c40a4699a2e
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-03-09 23:11:51 +0300

    feat: hide db pass connection data

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,22 @@
 import {Module} from "@nestjs/common";
 import {SequelizeModule} from "@nestjs/sequelize";
 import { UsersModule } from './users/users.module';
+import {ConfigModule} from "@nestjs/config";
 
 @Module({
     controllers: [],
     providers: [],
     imports: [
+        ConfigModule.forRoot({
+            envFilePath: '.env',
+        }),
         SequelizeModule.forRoot({
             dialect: 'postgres',
-            host: 'localhost',
-            port: 5432,
-            username: 'postgres',
-            password: '781227xyZ',
-            database: 'nest-course',
+            host: process.env.POSTGRES_HOST,
+            port: Number(process.env.POSTGRES_PORT),
+            username: process.env.POSTGRES_USER,
+            password: process.env.POSTGRES_PASSWORD,
+            database: process.env.POSTGRES_DB,
             models: [],
             autoLoadModels: true,
         }),
