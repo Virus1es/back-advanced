dc00f009caedc6b2be430266731804ac6abb60d6
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-03-10 21:29:57 +0300

    feat: connect table UserRoles

--- src/roles/roles.model.ts
+++ src/roles/roles.model.ts
@@ -1,6 +1,7 @@
 import {BelongsToMany, Column, DataType, Model, Table} from "sequelize-typescript";
 import {ApiProperty} from "@nestjs/swagger";
 import {User} from "../users/users.model";
+import {UserRoles} from "./user-roles.model";
 
 interface RoleCreationAttrs {
     value: string;
--- src/users/users.model.ts
+++ src/users/users.model.ts
@@ -1,6 +1,7 @@
 import {BelongsToMany, Column, DataType, Model, Table} from "sequelize-typescript";
 import {ApiProperty} from "@nestjs/swagger";
 import {Role} from "../roles/roles.model";
+import {UserRoles} from "../roles/user-roles.model";
 
 interface UserCreationAttrs {
     email: string;
