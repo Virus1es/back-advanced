5be6dfeb63044ace2636d453d78e0041b9a2d48a
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-03-10 21:16:52 +0300

    create: create model for roles

---
+++ src/roles/roles.model.ts
@@ -0,0 +1,23 @@
+import {Column, DataType, Model, Table} from "sequelize-typescript";
+import {ApiProperty} from "@nestjs/swagger";
+
+interface RoleCreationAttrs {
+    value: string;
+    description: string;
+}
+
+@Table({tableName: 'users'})
+export class Role extends Model<Role, RoleCreationAttrs> {
+    @ApiProperty({example: '1', description: 'Unique identifier'})
+    @Column({type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true})
+    declare id: number;
+
+    @ApiProperty({example: 'ADMIN', description: 'Name of user role'})
+    @Column({type: DataType.STRING, unique: true, allowNull: false})
+    value: string;
+
+    @ApiProperty({example: 'Administrator', description: 'Description of role'})
+    @Column({type: DataType.STRING, allowNull: false})
+    description: string;
+
+}
