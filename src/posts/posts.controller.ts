66d64e07d697d53750e2b30c321e2b4e661b1bc5
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-03-11 22:48:51 +0300

    feat: create route for posts

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Post, UploadedFile} from '@nestjs/common';
+import {CreatePostDto} from "./dto/create-post.dto";
+import {PostsService} from "./posts.service";
+import {ApiOperation, ApiResponse} from "@nestjs/swagger";
+import {Role} from "../roles/roles.model";
 
 @Controller('posts')
-export class PostsController {}
+export class PostsController {
+
+    constructor(private postsService: PostsService) {}
+
+    @ApiOperation({summary: 'Create post with image'})
+    @ApiResponse({status: 200, type: [Post]})
+    @Post()
+    createPost(@Body() dto: CreatePostDto,
+               @UploadedFile() image: string) {
+        return this.postsService.create(dto, image)
+    }
+}
