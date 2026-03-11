15446c3b2a8f0cc79c833b95b0526ff4b6566bb9
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-03-11 21:12:55 +0300

    docs: add docs to functions

--- src/roles/roles.controller.ts
+++ src/roles/roles.controller.ts
@@ -1,16 +1,25 @@
-import {Body, Controller, Get, Param, Post} from '@nestjs/common';
+import {Body, Controller, Get, Param, Post, UseGuards} from '@nestjs/common';
 import {RolesService} from "./roles.service";
 import {CreateRoleDto} from "./dto/create-role.dto";
+import {ApiOperation, ApiResponse} from "@nestjs/swagger";
+import {User} from "../users/users.model";
+import {Roles} from "../auth/roles-auth.decorator";
+import {RolesGuard} from "../auth/roles.guard";
+import {Role} from "./roles.model";
 
 @Controller('roles')
 export class RolesController {
     constructor(private roleService: RolesService) {}
 
+    @ApiOperation({summary: 'Create role'})
+    @ApiResponse({status: 200, type: [Role]})
     @Post()
     create(@Body() dto: CreateRoleDto) {
         return this.roleService.createRole(dto);
     }
 
+    @ApiOperation({summary: 'Find role by name(value)'})
+    @ApiResponse({status: 200, type: [Role]})
     @Get('/:value')
     getByValue(@Param('value') value: string) {
         return this.roleService.getRoleByValue(value);
