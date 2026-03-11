06278d985bff607908a1a731020fba53c3c19105
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-03-11 22:52:35 +0300

    feat: create function for create post without files logic

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import {CreatePostDto} from "./dto/create-post.dto";
+import {InjectModel} from "@nestjs/sequelize";
+import {Post} from "./posts.model";
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+
+    constructor(@InjectModel(Post) private postRepository: typeof Post) {}
+
+    async create(dto: CreatePostDto, image: string) {
+        const filename = 'aaaaa';
+        return this.postRepository.create({...dto, image: filename});
+    }
+}
