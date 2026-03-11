efc2f155dcbe3bb395db8c3e206c784e674b8344
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-03-11 21:31:52 +0300

    feat: add validators decorators to fields

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,9 +1,14 @@
 import {ApiProperty} from "@nestjs/swagger";
+import {IsEmail, IsString, Length} from "class-validator";
 
 export class CreateUserDto {
     @ApiProperty({example: 'user@mail.ru', description: 'User email'})
+    @IsString({message: 'That field must be string'})
+    @IsEmail({}, {message: 'Email is invalid'})
     readonly email: string;
 
     @ApiProperty({example: '12334241', description: 'User password'})
+    @IsString({message: 'That field must be string'})
+    @Length(4, 16, {message: 'Length are more then 4 and less tha 16'})
     readonly password: string;
 }
