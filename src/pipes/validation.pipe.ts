9e5b0e2ef7b6277f67ea38cc7506e81ebe910ddd
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-03-11 23:19:44 +0300

    fix: fix pipe

--- src/pipes/validation.pipe.ts
+++ src/pipes/validation.pipe.ts
@@ -1,4 +1,11 @@
-import {ArgumentMetadata, HttpException, HttpStatus, Injectable, PipeTransform} from "@nestjs/common";
+import {
+    ArgumentMetadata,
+    BadRequestException,
+    HttpException,
+    HttpStatus,
+    Injectable,
+    PipeTransform
+} from "@nestjs/common";
 import {plainToInstance} from "class-transformer";
 import {validate} from "class-validator";
 import {ValidationException} from "../exceptions/validation.exception";
@@ -7,24 +14,40 @@ import {ValidationException} from "../exceptions/validation.exception";
 @Injectable()
 export class ValidationPipe implements PipeTransform<any> {
     async transform(value: any, metadata: ArgumentMetadata): Promise<any> {
-        if (!metadata.metatype) {
-            throw new HttpException('No metadata send', HttpStatus.BAD_REQUEST);
+        if (!this.toValidate(metadata) || !metadata.metatype) {
+            return value;
         }
 
-        const obj = plainToInstance(metadata.metatype, value);
-        const errors = await validate(obj);
+        const object = plainToInstance(metadata.metatype, value);
 
-        if(errors.length) {
-            let messages = errors.map(err => {
+        if (!object || typeof object !== 'object') {
+            throw new BadRequestException('Invalid data format');
+        }
+
+        const errors = await validate(object);
+
+        if (errors.length > 0) {
+            const messages = errors.map(err => {
                 if (!err.constraints) {
                     return `${err.property} - Unknown error`;
                 }
-                return `${err.property} - ${Object.values(err.constraints).join(', ')}`
+                return `${err.property} - ${Object.values(err.constraints).join(', ')}`;
             });
-
-            throw new ValidationException(messages);
+            throw new BadRequestException(messages);
         }
 
-        return value;
+        return object;
+    }
+
+    private toValidate(metadata: ArgumentMetadata): boolean {
+        const { metatype } = metadata;
+        return (
+            metatype !== undefined &&
+            metatype !== String &&
+            metatype !== Boolean &&
+            metatype !== Number &&
+            metatype !== Array &&
+            metatype !== Object
+        );
     }
 }
