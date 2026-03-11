e4c7abf4fd49e05eeffc388b46a1bc4641ddef95
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-03-12 00:17:47 +0300

    feat: now server can return static images

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,6 +10,8 @@ import { AuthModule } from './auth/auth.module';
 import { PostsModule } from './posts/posts.module';
 import {Post} from "./posts/posts.model";
 import { FilesModule } from './files/files.module';
+import {ServeStaticModule} from "@nestjs/serve-static";
+import * as path from 'path';
 
 @Module({
     controllers: [],
@@ -27,6 +29,9 @@ import { FilesModule } from './files/files.module';
             database: process.env.POSTGRES_DB,
             models: [User, Role, UserRoles, Post],
             autoLoadModels: true,
+        }),
+        ServeStaticModule.forRoot({
+            rootPath: path.resolve(__dirname, '../static'),
         }),
         UsersModule,
         RolesModule,
