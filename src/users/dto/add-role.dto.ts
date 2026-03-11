091f8e0fbf23fe826c0a330e8836df08659ffb7b
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-03-11 21:15:50 +0300

    docs: add docs to dto's

--- src/users/dto/add-role.dto.ts
+++ src/users/dto/add-role.dto.ts
@@ -1,4 +1,9 @@
+import {ApiProperty} from "@nestjs/swagger";
+
 export class AddRoleDto {
+    @ApiProperty({example: 'ADMIN', description: 'Name of user role'})
     readonly value: string;
+
+    @ApiProperty({example: '1', description: 'User unique identifier'})
     readonly userId: number;
 }
--- src/users/dto/ban-user.dto.ts
+++ src/users/dto/ban-user.dto.ts
@@ -1,4 +1,9 @@
+import {ApiProperty} from "@nestjs/swagger";
+
 export class BanUserDto {
+    @ApiProperty({example: '1', description: 'User unique identifier'})
     readonly userId: number;
+
+    @ApiProperty({example: 'No enter too long', description: 'Why user are banned'})
     readonly banReason: string;
 }
