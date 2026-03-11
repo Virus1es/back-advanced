723d4d493cf303b965a3f64d1eaf6ca3078f542f
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-03-11 22:40:14 +0300

    feat: edit dto field

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -7,6 +7,6 @@ export class CreatePostDto {
     @ApiProperty({example: 'Some text', description: 'Content text in post'})
     readonly content: string;
 
-    @ApiProperty({example: 'image.jpg', description: 'Name of image that use in post'})
-    readonly image: string;
+    @ApiProperty({example: '1', description: 'Unique identifier of user who create this post'})
+    readonly userId: string;
 }
