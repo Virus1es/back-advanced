4d61d13eb75ebe5c0fc331ad8278dde3b975768e
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-03-12 00:47:07 +0300

    env: remove unused package

--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,14 +1,14 @@
 import {HttpException, HttpStatus, Injectable} from '@nestjs/common';
 import * as fs from 'fs';
 import * as path from 'path';
-import * as uuid from 'uuid';
+import { randomUUID } from 'crypto';
 
 @Injectable()
 export class FilesService {
 
     async createFile(file): Promise<string> {
         try{
-            const fileName = uuid.v4() + '.jpg';
+            const fileName = randomUUID() + '.jpg';
             const filePath = path.resolve(__dirname, '../../static');
             if(!fs.existsSync(filePath)){
                 fs.mkdirSync(filePath, {recursive: true});
