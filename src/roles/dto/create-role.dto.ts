86afcb5b200a29e075761c612a49b328689792f7
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-03-11 21:45:30 +0300

    feat: add decorators to validation to other dtos

--- src/roles/dto/create-role.dto.ts
+++ src/roles/dto/create-role.dto.ts
@@ -1,4 +1,13 @@
+import {ApiProperty} from "@nestjs/swagger";
+import {IsString} from "class-validator";
+
 export class CreateRoleDto {
+
+    @ApiProperty({example: 'ADMIN', description: 'Name of user role'})
+    @IsString({message: 'That field must be string'})
     readonly value: string;
+
+    @ApiProperty({example: 'Administrator in app', description: 'Description of role'})
+    @IsString({message: 'That field must be string'})
     readonly description: string;
 }
--- src/users/dto/add-role.dto.ts
+++ src/users/dto/add-role.dto.ts
@@ -1,9 +1,12 @@
 import {ApiProperty} from "@nestjs/swagger";
+import {IsString, IsNumber} from "class-validator";
 
 export class AddRoleDto {
     @ApiProperty({example: 'ADMIN', description: 'Name of user role'})
+    @IsString({message: 'That field must be string'})
     readonly value: string;
 
     @ApiProperty({example: '1', description: 'User unique identifier'})
+    @IsNumber({}, {message: 'That field must be number'})
     readonly userId: number;
 }
